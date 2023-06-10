c5bd11d00b20dfc5c6bd3b5f3e08d511ffc33066
Scott <saseaa37@gmail.com>
2023-06-10 13:13:39 -0600

    Fifth Commit

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import SignUpView
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
--- pages/urls.py
+++ pages/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.conf.urls.static import static
 
 from .views import HomePageView, AboutPageView, ContactPageView
 
