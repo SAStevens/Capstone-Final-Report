3baa8c58b41484de5f5b636a6c91354b143cb3a9
Scott <saseaa37@gmail.com>
2023-06-12 20:50:41 -0600

    Eleventh Commit

--- pages/urls.py
+++ pages/urls.py
@@ -5,6 +5,6 @@ from .views import HomePageView, AboutPageView, ContactPageView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
-    path("", AboutPageView.as_view(), name="about"),
-    path("", ContactPageView.as_view(), name="contact"),
+    path("about/", AboutPageView.as_view(), name="about"),
+    path("contact/", ContactPageView.as_view(), name="contact"),
 ]
